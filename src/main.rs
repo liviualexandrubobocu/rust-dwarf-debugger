da1ca6d66513563569ebee65d2e275562180be96
Liviu Bobocu <liviualexandrubobocu@gmail.com>
2023-12-03 00:53:37 +0200

    Working first draft without parsing the mmap in a file

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,28 @@ mod debug_data;
 mod source_maps;
 mod state_management;
 
+use io::Error;
 use std::env;
 use std::fs;
 
 use std::io::Read;
 use error::Result;
 
-use object::{Object, ObjectSection};
+use object::{Object, File as ObjectFile, ObjectSection};
 
 use std::process::Command;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::io::{self, BufRead};
+use gimli::LittleEndian;
+use memmap2::Mmap;
+
 
 
 fn main() -> Result<()> {
     let output = Command::new("wasm-tools")
-        .args(["print", "-p",  "C:\\Users\\liviu\\RustroverProjects\\rust-dwarf-debugger\\src\\test_wasm.wasm"])
+        .args(["dump", "C:\\Users\\liviu\\RustroverProjects\\rust-dwarf-debugger\\src\\test_wasm.wasm"])
         .output()
         .expect("Failed to execute command");
 
@@ -48,6 +52,36 @@ fn main() -> Result<()> {
     // wasm-tools dump /filename used to obtain dump
     // wasm-tools addr2line -- C:\\Users\\liviu\\RustroverProjects\\rust-dwarf-debugger\\src\\bitpack.wasm 0x110e2 used to map back information to debug lines
 
+    let file_path = "C:\\Users\\liviu\\RustroverProjects\\rust-dwarf-debugger\\src\\test_wasm.wasm"; // replace with your file path
+    let data = read_wasm_file(file_path)?;
 
+    let mut file = File::open(file_path)?;
+
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+
+    let mmap = unsafe { Mmap::map(&file)?  };
+
+
+    parse_wasm_header(&data)?;
+
+
+    wasm_parser::parse_wasm(&data, &mmap, LittleEndian).unwrap();
+    Ok(())
+}
+
+fn read_wasm_file(file_path: &str) -> std::io::Result<Vec<u8>> {
+    fs::read(file_path)
+}
+
+fn parse_wasm_header(data: &[u8]) -> Result<(), std::io::Error> {
+    if data.len() < 8 {
+        return Err(Error::new(io::ErrorKind::Other, "File too short"));
+    }
+    if &data[0..4] != b"\0asm" {
+        return Err(Error::new(io::ErrorKind::Other, "Not a valid wasm file"));
+    }
+    let version = u32::from_le_bytes([data[4], data[5], data[6], data[7]]);
+    println!("WASM version: {}", version);
     Ok(())
 }
--- src/wasm_parser.rs
+++ src/wasm_parser.rs
@@ -2,12 +2,13 @@ use std::borrow;
 use wasmparser::{Parser, Payload};
 use crate::error::Result;
 use gimli::{DebugAbbrev, DebugInfo, DebugLine, LittleEndian, UnitOffset, AttributeValue, DebuggingInformationEntry, EndianSlice, EntriesTreeNode, constants, RunTimeEndian, BigEndian};
-use object::{Endian, Object, ObjectSection};
+use memmap2::Mmap;
+use object::{Endian, File, Object, ObjectSection};
 use crate::debug_data::{DebugInfoStorage, Function, Variable};
 use crate::source_maps::{SourceMapping};
 
-pub fn parse_wasm(wasm_contents: &[u8],  object: &object::File,
-                  endian: gimli::RunTimeEndian) -> Result<()> {
+pub fn parse_wasm(wasm_contents: &[u8], object: &Mmap,
+                  endian: LittleEndian) -> Result<()> {
     let parser = Parser::new(0);
     for payload in parser.parse_all(wasm_contents){
         match payload? {
@@ -16,7 +17,7 @@ pub fn parse_wasm(wasm_contents: &[u8],  object: &object::File,
             },
             Payload::CustomSection { name, data_offset: _data_offset, data: _data, range } => {
                 if is_dwarf_section(name) {
-                    let result = handle_dwarf_section(name, _data, object, endian);
+                    let result = handle_dwarf_section(name, _data, &object, endian);
                 }
             },
             _ => {}
@@ -31,7 +32,7 @@ fn is_dwarf_section(name: &str) -> bool {
     matches!(name, ".debug_info" | ".debug_line" | ".debug_abbrev" | ".debug_str" | ".debug_ranges" | ".debug_pubtypes" | ".debug_pubnames")
 }
 
-fn handle_dwarf_section(name: &str, data: &[u8], object: &object::File, endianness: gimli::RunTimeEndian) -> Result<()> {
+fn handle_dwarf_section(name: &str, data: &[u8], object: &Mmap, endianness: LittleEndian) -> Result<()> {
     match name {
         ".debug_info" => {
             let _debug_info = parse_debug_info_section(data, object, endianness);
@@ -45,7 +46,7 @@ fn handle_dwarf_section(name: &str, data: &[u8], object: &object::File, endianne
     Ok(())
 }
 
-fn parse_debug_info_section(data: &[u8], object: &object::File, endianness: gimli::RunTimeEndian) -> Result<(), Box<dyn std::error::Error>> {
+fn parse_debug_info_section(data: &[u8], object: &Mmap, endianness: LittleEndian) -> Result<(), Box<dyn std::error::Error>> {
     let debug_info = DebugInfo::new(data, endianness);
     let mut debug_info_storage = DebugInfoStorage {
         functions: Vec::new(),
@@ -59,51 +60,51 @@ fn parse_debug_info_section(data: &[u8], object: &object::File, endianness: giml
         let unit_offset = unitHeader.offset().as_debug_info_offset().ok_or(());
         let offset = UnitOffset(unit_offset.unwrap());
         let mut unit = unitHeader.entries_tree(&abbrevs, offset.0.to_unit_offset(&unitHeader))?;
-        let load_section = |id: gimli::SectionId| -> Result<borrow::Cow<[u8]>, gimli::Error> {
-            match object.section_by_name(id.name()) {
-                Some(ref section) => Ok(section
-                    .uncompressed_data()
-                    .unwrap_or(borrow::Cow::Borrowed(&[][..]))),
-                None => Ok(borrow::Cow::Borrowed(&[][..])),
-            }
-        };
+        // let load_section = |id: gimli::SectionId| -> Result<borrow::Cow<[u8]>, gimli::Error> {
+        //     match object.section_by_name(id.name()) {
+        //         Some(ref section) => Ok(section
+        //             .uncompressed_data()
+        //             .unwrap_or(borrow::Cow::Borrowed(&[][..]))),
+        //         None => Ok(borrow::Cow::Borrowed(&[][..])),
+        //     }
+        // };
 
         // Load all of the sections.
-        let dwarf_cow = gimli::Dwarf::load(&load_section)?;
+        //let dwarf_cow = gimli::Dwarf::load(&load_section)?;
 
-        // Borrow a `Cow<[u8]>` to create an `EndianSlice`.
-        let borrow_section: &dyn for<'a> Fn(
-            &'a borrow::Cow<[u8]>,
-        ) -> gimli::EndianSlice<'a, gimli::RunTimeEndian> =
-            &|section| gimli::EndianSlice::new(&*section, endianness);
-
-        // Create `EndianSlice`s for all of the sections.
-        let dwarf = dwarf_cow.borrow(&borrow_section);
-
-        let mut iter = dwarf.units();
-        while let Some(header) = iter.next()? {
-            println!(
-                "Unit at <.debug_info+0x{:x}>",
-                header.offset().as_debug_info_offset().unwrap().0
-            );
-            let unit = dwarf.unit(header)?;
-
-            // Iterate over the Debugging Information Entries (DIEs) in the unit.
-            let mut depth = 0;
-            let mut entries = unit.entries();
-            while let Some((delta_depth, entry)) = entries.next_dfs()? {
-                depth += delta_depth;
-                println!("<{}><{:x}> {}", depth, entry.offset().0, entry.tag());
-                parse_entry(data, entry, &mut debug_info_storage)?;
-
-
-                // Iterate over the attributes in the DIE.
-                let mut attrs = entry.attrs();
-                while let Some(attr) = attrs.next()? {
-                    println!("   {}: {:?}", attr.name(), attr.value());
-                }
-            }
-        }
+        // // Borrow a `Cow<[u8]>` to create an `EndianSlice`.
+        // let borrow_section: &dyn for<'a> Fn(
+        //     &'a borrow::Cow<[u8]>,
+        // ) -> gimli::EndianSlice<'a, gimli::RunTimeEndian> =
+        //     &|section| gimli::EndianSlice::new(&*section, endianness);
+        //
+        // // Create `EndianSlice`s for all of the sections.
+        // let dwarf = dwarf_cow.borrow(&borrow_section);
+        //
+        // let mut iter = dwarf.units();
+        // while let Some(header) = iter.next()? {
+        //     println!(
+        //         "Unit at <.debug_info+0x{:x}>",
+        //         header.offset().as_debug_info_offset().unwrap().0
+        //     );
+        //     let unit = dwarf.unit(header)?;
+        //
+        //     // Iterate over the Debugging Information Entries (DIEs) in the unit.
+        //     let mut depth = 0;
+        //     let mut entries = unit.entries();
+        //     while let Some((delta_depth, entry)) = entries.next_dfs()? {
+        //         depth += delta_depth;
+        //         println!("<{}><{:x}> {}", depth, entry.offset().0, entry.tag());
+        //         parse_entry(data, entry, &mut debug_info_storage)?;
+        //
+        //
+        //         // Iterate over the attributes in the DIE.
+        //         let mut attrs = entry.attrs();
+        //         while let Some(attr) = attrs.next()? {
+        //             println!("   {}: {:?}", attr.name(), attr.value());
+        //         }
+        //     }
+        // }
 
     }
 
